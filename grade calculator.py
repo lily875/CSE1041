13bbc78e9ddeda0716a4ba11baa3d1d4b8c026bc
lily875 <115868209+lily875@users.noreply.github.com>
2026-02-25 14:40:55 +0530

    Add grade classification based on student marks

---
+++ grade calculator.py
@@ -0,0 +1,9 @@
+marks=int(input("enter the student marks"))
+if marks>=60 :
+    print("first class")
+elif marks>=50:
+    print("second class")
+elif marks>=40:
+    print("third class")
+else :
+    print("the student has failed ")
